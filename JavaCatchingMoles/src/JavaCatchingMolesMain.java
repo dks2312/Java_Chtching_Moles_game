1e8069ac991cba6e121193b8ac6818e61f634227
dks2312 <dtg2312@gmail.com>
2022-05-17 16:59:42 +0900

    update javaCatcingMoles 코드 정리

--- JavaCatchingMoles/src/JavaCatchingMolesMain.java
+++ JavaCatchingMoles/src/JavaCatchingMolesMain.java
@@ -7,45 +7,39 @@ import java.awt.event.MouseEvent;
 
 public class JavaCatchingMolesMain extends JFrame{
 	static int score = 0;
-	JLabel la = new JLabel("score:0");
+	JLabel la;
+	JButton[] moless;
 	
 	public JavaCatchingMolesMain()  {
 		setTitle("자바버전 추억의 두더지 잡기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
 		Container c = getContentPane();
 		c.setLayout(null);
-		
 
-		la.setSize(100, 50);
-		la.setLocation(200, 0);
-		c.add(la);
-		
-		JButton[] moless = new JButton[7]; 
-		for(int i = 0; i < moless.length; i++) {
-			ImageIcon img = new ImageIcon("images/moles.png");
-			ImageIcon img1 = new ImageIcon("images/moles1.png");
-			
-			moless[i] = new JButton(img);
-			moless[i].setSize(50, 80);
-			moless[i].addMouseListener(new addScore());
-			moless[i].setPressedIcon(img1);
-			c.add(moless[i]);
-		}	
-		
-		moless[0].setLocation(50, 100);
-		moless[1].setLocation(150, 100);
-		moless[2].setLocation(250, 100);
-		moless[3].setLocation(350, 100);
-		moless[4].setLocation(100, 150);
-		moless[5].setLocation(200, 150);
-		moless[6].setLocation(300, 150);
+		gameInit(c);
 				
 		setSize(500, 300);
 		setVisible(true);	
 		setResizable(false);
 		
+		gameStart();
+		
+		gameEnd(c);	
+	}
+
+	private void gameEnd(Container c) {		
+		la.setText("100점 축하드립니다!!");
+		la.setSize(200, 50);
+		la.setLocation(200, 0);
+		c.add(la);
+		
+		for(int i = 0; i < moless.length; i++) {
+			moless[i].setEnabled(true);
+		}
+	}
+
+	private void gameStart() {
 		while(score < 100) {
 			int rand = (int)(Math.random()*moless.length);
 			moless[rand].setEnabled(true);
@@ -57,29 +51,44 @@ public class JavaCatchingMolesMain extends JFrame{
 				e.printStackTrace();
 			}
 			
+			la.setText("score: "+ score);
 			for(int i = 0; i < moless.length; i++) {
 				moless[i].setEnabled(false);
 			}		
 		}
-		
-		la = new JLabel("100점 축하합니다!!!");
+	}
+
+	private void gameInit(Container c) {
+		la = new JLabel("score: 0");
 		la.setSize(100, 50);
 		la.setLocation(200, 0);
 		c.add(la);
 		
+		moless = new JButton[7]; 
 		for(int i = 0; i < moless.length; i++) {
-			moless[i].setEnabled(true);
+			ImageIcon img = new ImageIcon("images/moles.png");
+			ImageIcon img1 = new ImageIcon("images/moles1.png");
+			
+			moless[i] = new JButton(img);
+			moless[i].setSize(50, 80);
+			moless[i].addMouseListener(new molesAction());
+			moless[i].setPressedIcon(img1);
+			c.add(moless[i]);
 		}	
+		
+		moless[0].setLocation(50, 100);
+		moless[1].setLocation(150, 100);
+		moless[2].setLocation(250, 100);
+		moless[3].setLocation(350, 100);
+		moless[4].setLocation(100, 150);
+		moless[5].setLocation(200, 150);
+		moless[6].setLocation(300, 150);
 	}
 	
-	private class addScore extends MouseAdapter{
-		public void mousePressed(MouseEvent e) {
+	private class molesAction extends MouseAdapter{
+		public void mousePressed(MouseEvent e) {	
 			JButton b = (JButton)e.getSource();
-			
-			if(!b.isEnabled()) return;
-			
-			score+=10;
-			la.setText("score: "+ score);
+			if(b.isEnabled()) score+=10;
 		}
 		
 		public void mouseReleased(MouseEvent e) {
